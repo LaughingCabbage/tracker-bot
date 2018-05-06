7ba750ee1d94dc31bb4db1da9161c5a8cee54c93
Kevin Gentile <kgentile@protonmail.com>
2018-05-05 20:26:37 -0400

    make test resoruce a constant

--- constant.go
+++ constant.go
@@ -2,3 +2,6 @@ package main
 
 //ForniteTrackerFileName stores the file name for a file containing the fortnite tracker api token
 const ForniteTrackerFileName = ".tracker-key"
+
+//TestResourcePath holds the path to test resources for used by the project
+const TestResourcePath = "test/"
--- resource_test.go
+++ resource_test.go
@@ -8,7 +8,7 @@ func TestLoadKey(t *testing.T) {
 	//value taken directly from .test-tracker-key
 	testKey := Key{Value: "1234123412341234ASDFasdf"}
 
-	key := LoadKey("test/" + ForniteTrackerFileName)
+	key := LoadKey(TestResourcePath + ForniteTrackerFileName)
 
 	if key != testKey {
 		t.Fail()
